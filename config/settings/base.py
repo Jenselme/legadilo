aca52ad9b12d9ad616a7edecf04613ca165098bb
Julien Enselme <jenselme@jujens.eu>
2023-12-08 09:31:37 +0100

    chore: configure database statement timeout

--- config/settings/base.py
+++ config/settings/base.py
@@ -46,6 +46,8 @@ LOCALE_PATHS = [str(BASE_DIR / "locale")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {"default": env.db("DATABASE_URL")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
+# https://blog.heroku.com/postgres-essentials#set-a-code-statement_timeout-code-for-web-dynos
+DATABASES["OPTIONS"] = {"options": "-c statement_timeout=30000"}
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
