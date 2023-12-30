e75144a2fe16a539285ac86d6fd542ccee514226
Julien Enselme <jenselme@jujens.eu>
2023-12-30 12:44:01 +0000

    feat: add function to sanitize while keeping safe tags

--- legadilo/utils/security.py
+++ legadilo/utils/security.py
@@ -3,3 +3,7 @@ import nh3
 
 def full_sanitize(data: str) -> str:
     return nh3.clean(data, tags=set(), strip_comments=True)
+
+
+def sanitize_keep_safe_tags(data: str) -> str:
+    return nh3.clean(data, tags=nh3.ALLOWED_TAGS, attributes=nh3.ALLOWED_ATTRIBUTES, strip_comments=True)
--- legadilo/utils/tests/test_security.py
+++ legadilo/utils/tests/test_security.py
@@ -1,6 +1,6 @@
 import pytest
 
-from legadilo.utils.security import full_sanitize
+from legadilo.utils.security import full_sanitize, sanitize_keep_safe_tags
 
 
 @pytest.mark.parametrize(
@@ -14,5 +14,21 @@ from legadilo.utils.security import full_sanitize
 )
 def test_full_sanitize(data, clean_data):
     cleaned_data = full_sanitize(data)
+
+    assert cleaned_data == clean_data
+
+
+@pytest.mark.parametrize(
+    ("data", "clean_data"),
+    [
+        pytest.param("", "", id="empty-string"),
+        pytest.param("<p>Test</p>", "<p>Test</p>", id="basic-html"),
+        pytest.param("<div>Test <p>complete</p></div>", "<div>Test <p>complete</p></div>", id="nested-html"),
+        pytest.param("<div>Hello", "<div>Hello</div>", id="invalid-html"),
+        pytest.param("<script>alert('hell')</script><p>Coucou</p>", "<p>Coucou</p>", id="with-script"),
+    ],
+)
+def test_sanitize_keep_safe_tags(data, clean_data):
+    cleaned_data = sanitize_keep_safe_tags(data)
 
     assert cleaned_data == clean_data
