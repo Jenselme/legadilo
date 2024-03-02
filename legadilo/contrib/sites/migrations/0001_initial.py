de43bfed27a6098de35afcae722fa91ebef7e269
Julien Enselme <jenselme@jujens.eu>
2024-03-02 12:57:42 +0100

    chore: update deps

--- legadilo/contrib/sites/migrations/0001_initial.py
+++ legadilo/contrib/sites/migrations/0001_initial.py
@@ -1,5 +1,5 @@
 import django.contrib.sites.models
-from django.contrib.sites.models import _simple_domain_name_validator
+from django.contrib.sites.models import _simple_domain_name_validator  # noqa: PLC2701 Private name import
 from django.db import migrations, models
 
 
