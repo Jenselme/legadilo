7b44e7c5cb1b815e45ec21b456b4afefbfee5352
Julien Enselme <jenselme@jujens.eu>
2024-03-12 22:47:27 +0100

    ref: simplify the admin
    I don’t expect to have complex logic in here.

--- legadilo/feeds/admin.py
+++ legadilo/feeds/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
+from legadilo.feeds.models.article import Article
 from legadilo.feeds.models.feed import Feed
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Feed)
