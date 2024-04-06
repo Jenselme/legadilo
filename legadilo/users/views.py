c11bc20e8e4fe18a1ce09d4088f42328c768abfe
Julien Enselme <jenselme@jujens.eu>
2024-04-06 19:02:48 +0200

    feat: redirect to the default reading list after login

--- legadilo/users/views.py
+++ legadilo/users/views.py
@@ -38,7 +38,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
     permanent = False
 
     def get_redirect_url(self):
-        return reverse("users:detail", kwargs={"pk": self.request.user.pk})
+        return reverse("feeds:default_reading_list")
 
 
 user_redirect_view = UserRedirectView.as_view()
