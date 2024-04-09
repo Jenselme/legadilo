ca8bf9993e98e3db18b256cf39cdf2db46a9fa60
Julien Enselme <jenselme@jujens.eu>
2024-04-09 22:02:22 +0200

    ref: merge template & code for list of articles views

--- legadilo/feeds/views/__init__.py
+++ legadilo/feeds/views/__init__.py
@@ -1,7 +1,6 @@
 from .article_views import article_details_view, update_article_view
-from .reading_list_with_articles_view import reading_list_with_articles_view
+from .list_of_articles_views import reading_list_with_articles_view, tag_with_articles_view
 from .subscribe_to_feed_view import subscribe_to_feed_view
-from .tags_views import tag_with_articles_view
 
 __all__ = [
     "article_details_view",
--- legadilo/feeds/views/list_of_articles_views.py
+++ legadilo/feeds/views/list_of_articles_views.py
@@ -1,10 +1,14 @@
+from typing import Any
+
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 from django.views.decorators.http import require_GET
 
-from legadilo.feeds.models import Article, ReadingList
+from legadilo.feeds.models import Article, ReadingList, Tag
 from legadilo.users.typing import AuthenticatedHttpRequest
 from legadilo.utils.pagination import get_requested_page
 from legadilo.utils.validators import get_page_number_from_request
@@ -24,7 +28,19 @@ def reading_list_with_articles_view(
             return HttpResponseNotFound()
         return HttpResponseRedirect(reverse("feeds:default_reading_list"))
 
-    articles_paginator = Article.objects.get_articles_of_reading_list(displayed_reading_list)
+    return _display_list_of_articles(
+        request,
+        Article.objects.get_articles_of_reading_list(displayed_reading_list),
+        {
+            "page_title": displayed_reading_list.name,
+            "displayed_reading_list_id": displayed_reading_list.id,
+        },
+    )
+
+
+def _display_list_of_articles(
+    request: AuthenticatedHttpRequest, articles_paginator, page_ctx: dict[str, Any]
+) -> TemplateResponse:
     requested_page = get_page_number_from_request(request)
     articles_page = get_requested_page(articles_paginator, requested_page)
     reading_lists = ReadingList.objects.get_all_for_user(request.user)
@@ -32,15 +48,34 @@ def reading_list_with_articles_view(
 
     return TemplateResponse(
         request,
-        "feeds/reading_list_with_articles.html",
+        "feeds/list_of_articles.html",
         {
+            **page_ctx,
             "base": {
                 "fluid_content": True,
             },
             "reading_lists": reading_lists,
             "count_articles_of_reading_lists": count_articles_of_reading_lists,
-            "displayed_reading_list": displayed_reading_list,
             "articles_page": articles_page,
             "articles_paginator": articles_paginator,
+        },
+    )
+
+
+@require_GET
+@login_required
+def tag_with_articles_view(request: AuthenticatedHttpRequest, tag_slug: str) -> TemplateResponse:
+    displayed_tag = get_object_or_404(
+        Tag,
+        slug=tag_slug,
+        user=request.user,
+    )
+
+    return _display_list_of_articles(
+        request,
+        Article.objects.get_articles_of_tag(displayed_tag),
+        {
+            "page_title": _("Articles with tag '%(tag_name)s'") % {"tag_name": displayed_tag.name},
+            "displayed_reading_list_id": None,
         },
     )
