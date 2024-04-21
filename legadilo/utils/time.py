73440f6f453ce97ee3757915532832c878c108bb
Julien Enselme <jenselme@jujens.eu>
2024-04-21 18:30:37 +0200

    feat(utils): add a safe datetime parser based on dateutils

--- legadilo/utils/time.py
+++ legadilo/utils/time.py
@@ -1,5 +1,8 @@
 from datetime import UTC, datetime
 
+from dateutil.parser import ParserError
+from dateutil.parser import parse as datetime_parse
+
 
 def dt_to_http_date(dt: datetime) -> str:
     utc_dt = dt.astimezone(UTC)
@@ -20,3 +23,10 @@ def utcdt(  # noqa: PLR0913,PLR0917 (too many arguments)
     microsecond: int = 0,
 ) -> datetime:
     return datetime(year, month, day, hour, minute, second, microsecond, tzinfo=UTC)
+
+
+def safe_datetime_parse(data: str) -> datetime | None:
+    try:
+        return datetime_parse(data).astimezone(UTC)
+    except (ParserError, OverflowError):
+        return None
