f658b4c9d17892d742762b556e1974fc91a2e5a3
Julien Enselme <jenselme@jujens.eu>
2024-04-28 21:04:18 +0200

    ref(article): rename article views into article actions views

--- legadilo/feeds/views/__init__.py
+++ legadilo/feeds/views/__init__.py
@@ -1,8 +1,8 @@
-from .article_details_views import article_details_view, update_article_tags_view
-from .article_views import (
+from .article_actions_views import (
     delete_article_view,
     update_article_view,
 )
+from .article_details_views import article_details_view, update_article_tags_view
 from .fetch_article_views import add_article_view, refetch_article_view
 from .list_of_articles_views import reading_list_with_articles_view, tag_with_articles_view
 from .subscribe_to_feed_view import subscribe_to_feed_view
