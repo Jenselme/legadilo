33b60400a405b286f1ed195c78b892cea253db61
Julien Enselme <jenselme@jujens.eu>
2024-05-03 21:25:57 +0200

    feat(admin): enable autocompletion where needed

--- legadilo/feeds/admin.py
+++ legadilo/feeds/admin.py
@@ -16,23 +16,29 @@ from legadilo.feeds.models import (
 
 class ArticleTagInline(admin.TabularInline):
     model = ArticleTag
+    autocomplete_fields = ["tag"]
 
 
 class FeedTagInline(admin.TabularInline):
     model = FeedTag
+    autocomplete_fields = ["tag"]
 
 
 class ReadingListTagInline(admin.TabularInline):
     model = ReadingListTag
+    autocomplete_fields = ["tag"]
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ["name"]
+    autocomplete_fields = ["user"]
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+    autocomplete_fields = ["user"]
     inlines = [
         ArticleTagInline,
     ]
@@ -40,6 +46,9 @@ class ArticleAdmin(admin.ModelAdmin):
 
 @admin.register(Feed)
 class FeedAdmin(admin.ModelAdmin):
+    search_fields = ["title", "category__name"]
+    autocomplete_fields = ["user", "category"]
+    list_filter = ["feed_type"]
     inlines = [
         FeedTagInline,
     ]
@@ -47,11 +56,14 @@ class FeedAdmin(admin.ModelAdmin):
 
 @admin.register(FeedCategory)
 class FeedCategoryAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ["name"]
+    autocomplete_fields = ["user"]
 
 
 @admin.register(ReadingList)
 class ReadingListAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    autocomplete_fields = ["user"]
     inlines = [
         ReadingListTagInline,
     ]
@@ -59,9 +71,14 @@ class ReadingListAdmin(admin.ModelAdmin):
 
 @admin.register(FeedUpdate)
 class FeedUpdateAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ["feed__title"]
+    autocomplete_fields = ["feed"]
+    list_display = ["__str__", "created_at"]
+    list_filter = ["status"]
+    ordering = ["created_at"]
 
 
 @admin.register(FeedArticle)
 class FeedArticleAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ["feed__title", "article__title"]
+    autocomplete_fields = ["feed", "article"]
