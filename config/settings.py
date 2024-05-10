4a245ce43753dff84eed15281a366302615c836e
Julien Enselme <jenselme@jujens.eu>
2024-05-10 13:42:58 +0200

    feat: add import_export app

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,7 @@ LOCAL_APPS = [
     "legadilo.website",
     "legadilo.reading",
     "legadilo.feeds",
+    "legadilo.import_export",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
