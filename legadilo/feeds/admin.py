b281e32f7b75ac9332d8959748c60d5adcad6cd5
Julien Enselme <jenselme@jujens.eu>
2024-05-26 22:25:11 +0200

    feat(feed): add feed update as inline on admin page

--- legadilo/feeds/admin.py
+++ legadilo/feeds/admin.py
@@ -14,22 +14,6 @@ class FeedTagInline(admin.TabularInline):
     autocomplete_fields = ["tag"]
 
 
-@admin.register(Feed)
-class FeedAdmin(admin.ModelAdmin):
-    search_fields = ["title", "category__title"]
-    autocomplete_fields = ["user", "category"]
-    list_filter = ["enabled", "feed_type"]
-    inlines = [
-        FeedTagInline,
-    ]
-
-
-@admin.register(FeedCategory)
-class FeedCategoryAdmin(admin.ModelAdmin):
-    search_fields = ["title"]
-    autocomplete_fields = ["user"]
-
-
 @admin.register(FeedUpdate)
 class FeedUpdateAdmin(admin.ModelAdmin):
     search_fields = ["feed__title"]
@@ -37,6 +21,38 @@ class FeedUpdateAdmin(admin.ModelAdmin):
     list_display = ["__str__", "created_at"]
     list_filter = ["status"]
     ordering = ["created_at"]
+
+
+class FeedUpdateInline(admin.TabularInline):
+    model = FeedUpdate
+    readonly_fields = ("created_at",)
+    fields = ("created_at", "status", "error_message", "feed_last_modified", "feed_etag")
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Feed)
+class FeedAdmin(admin.ModelAdmin):
+    search_fields = ["title", "category__title"]
+    autocomplete_fields = ["user", "category"]
+    list_filter = ["enabled", "feed_type"]
+    inlines = [
+        FeedTagInline,
+        FeedUpdateInline,
+    ]
+
+
+@admin.register(FeedCategory)
+class FeedCategoryAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+    autocomplete_fields = ["user"]
 
 
 @admin.register(FeedArticle)
