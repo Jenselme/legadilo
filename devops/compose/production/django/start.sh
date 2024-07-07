3820439c70f05cbd1156a88d7a76159300229371
Julien Enselme <jenselme@jujens.eu>
2024-07-07 18:01:27 +0200

    chore: correct daphne port

--- devops/compose/production/django/start.sh
+++ devops/compose/production/django/start.sh
@@ -10,5 +10,5 @@ python manage.py migrate
 if [[ "${BUILD_ENV}" == "local" ]]; then
     exec python manage.py runserver 0.0.0.0:8000
 else
-    exec daphne config.asgi:application --bind 0.0.0.0 --port  --no-server-name
+    exec daphne config.asgi:application --bind 0.0.0.0 --port 8000 --no-server-name
 fi
