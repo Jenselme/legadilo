9e6e5770adb80d2cc972fbccc81faffa4c12db94
Julien Enselme <jenselme@jujens.eu>
2024-09-08 16:00:51 +0200

    chore: log message when daphne is starting
    This will help to know when this occurs when looking at production logs.

--- devops/compose/production/django/start.sh
+++ devops/compose/production/django/start.sh
@@ -7,8 +7,10 @@ set -o nounset
 python manage.py createcachetable
 python manage.py migrate
 
+SERVER_PORT=8000
+echo "Starting server on port ${SERVER_PORT}"
 if [[ "${BUILD_ENV}" == "local" ]]; then
-    exec python manage.py runserver 0.0.0.0:8000
+    exec python manage.py runserver 0.0.0.0:${SERVER_PORT}
 else
-    exec daphne config.asgi:application --bind 0.0.0.0 --port 8000 --no-server-name
+    exec daphne config.asgi:application --bind 0.0.0.0 --port ${SERVER_PORT} --no-server-name --verbosity 1
 fi
