cb267ba9ba83d86055cb49ef81e68bb1b977258c
Julien Enselme <jenselme@jujens.eu>
2024-10-12 15:45:53 +0200

    fix(article): correct technical date when updating article

--- legadilo/reading/models/article.py
+++ legadilo/reading/models/article.py
@@ -808,7 +808,7 @@ class Article(models.Model):
             self.content = article_data.content
             self.table_of_content = article_data.table_of_content
 
-        self.article_to_update = utcnow()
+        self.obj_updated_at = utcnow()
 
         return True
 
