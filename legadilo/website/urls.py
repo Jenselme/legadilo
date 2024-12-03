9a7246c242d58221ef4489ad51d6fd4818b2da97
Julien Enselme <jenselme@jujens.eu>
2024-12-03 22:29:56 +0100

    feat(website): add privacy policy
    It’s required to publish the extension on the Chrome webstore.

--- legadilo/website/urls.py
+++ legadilo/website/urls.py
@@ -23,6 +23,7 @@ app_name = "website"
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="website/home.html"), name="home"),
+    path("privacy/", TemplateView.as_view(template_name="website/privacy.html"), name="privacy"),
     path("manifest.json", views.manifest_view, name="manifest"),
     path("favicon.ico", views.default_favicon_view, name="favicon"),
 ]
