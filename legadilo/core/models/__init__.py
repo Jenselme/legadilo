30b36d045daa4898db2c605ffcff0d6a85322b5e
Julien Enselme <jenselme@jujens.eu>
2026-02-22 16:49:34 +0100

    feat: use same case-insensitive collation name as in sqlite
    - It comes by default in sqlite and mustn’t be created.
    - It’s not needed on Mysql/Mariadb
    - Having the same name for all makes querying easier.
    - Abstract the collation more cleanly in a dedication db field.
    
    See #150

--- legadilo/core/models/__init__.py
+++ legadilo/core/models/__init__.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-from django.db.models import CharField, DateTimeField
+from django.db.models import CharField, DateTimeField, EmailField
 from django.db.models.functions import Extract, Length
 
 from .timezone import Timezone
@@ -10,6 +10,12 @@ from .timezone import Timezone
 __all__ = ["Timezone"]
 
 CharField.register_lookup(Length)
+
+
+class CaseInsensitiveEmailField(EmailField):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.db_collation = "nocase"
 
 
 class ExtractTs(Extract):
